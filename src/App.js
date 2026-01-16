b279f36de8159a9f16dbc8aa48f023543492ca18
Lucky Sain <114669843+lucky-the-racer@users.noreply.github.com>
2026-01-16 16:55:01 +0530

    Update deployment technology mention in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1> CI/CD Pipeline Working!</h1>
-      <p>Deployed using Jenkins + Docker + AWS EC2</p>
+      <p>Deployed using Jenkins + Docker23 + AWS EC2</p>
     </div>
   );
 }
