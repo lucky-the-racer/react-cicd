2b3839b73b57a9388f2eabd0ee016b9658fc4116
EC2 Default User <ec2-user@ip-172-31-7-13.ap-south-1.compute.internal>
2026-01-16 11:29:19 +0000

    ui change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 function App() {
   return (
     <div>
-      <h1> CI/CD Pipeline Working!</h1>
-      <p>Deployed using Jenkins + Docker23 + AWS EC2</p>
+      <h1>CI/CD Pipeline Working!</h1>
+      <p>Deployed using Jenkins + Docker3 + AWS EC2</p>
     </div>
   );
 }
+
 export default App;
 
